fa9f21ba85762777bf02f8d51c5af9bdb4d85902
thimanshu756 <himanshu1662.be22@gmail.com>
2023-10-27 00:13:29 +0530

    home page

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import logo from "./Logo.png";
+import "./component.css"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar border h-14 bg-black'>
+        <div className='logo'>
+            <span className='flex text-white relative'>
+            <img src={logo} alt="" className='h-14 absolute '/>
+            <p className='absolute left-12 text-xl top-3'>Romi Finance</p>
+            </span>
+        </div>
+        <div className='lists text-white absolute right-[100px] top-4 justify-between w-[900px]'>
+            <ul className='flex justify-around'>
+            <li>Home</li>
+            <li>Dashboard</li>
+            <li>NFT</li>
+            <li>Earn</li>
+            <li>Buy</li>
+            <li>Referals</li>
+            <li>Ecosystem</li>
+            <li>About</li>
+            <li>
+              <button className=' bg-green-600 w-20 h-7 rounded-md'>Trade</button>
+            </li>
+            <li> <button className='border w-[150px] h-[30px]'>Connect Wallet</button></li>
+            <li>
+              <button>eth</button>
+            </li>
+         </ul>    
+        </div>
+    </div>
+
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-
+import Navbar from '../Components/Navbar';
+import costimg from './costs.svg';
+import trade from './trading.svg';
+import interest from './interest.svg';
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-black  h-[1000px] text-white'>
+      <div>
+        <Navbar />
+      </div>
+      <div className='bg-black '>
+        <div className='Sec absolute top-[120px] left-[100px] '>
+          <h1 className='text-6xl font-bold'>Decentralized <br />
+            Perpetual Exchange</h1> <br />
+          <p className='text-xl'>Trade BTC, ETH, AVAX and other top <br /> cryptocurrencies with up to 30x leverage <br /> directly from your wallet</p> <br />
+          <button className='border h-[40px] w-[170px] bg-green-600 rounded'>Launch Exchange</button>
+          <div className='cards flex justify-around gap-10 absolute top-96  '>
+            <div className='card border flex h-[120px] w-[400px] p-8 relative'>
+              <img src={trade} alt="" className='h-12' />
+              <div>
+                <p className='absolute left-28'>Total Trading Volume</p>
+                <p className='absolute top-14 left-28 text-3xl'>$0</p>
+              </div>
+            </div>
+            <div className='card border flex h-[120px] w-[400px] p-8 relative'>
+              <img src={trade} alt="" className='h-12' />
+              <div>
+                <p className='absolute left-28'>Total Trading Volume</p>
+                <p className='absolute top-14 left-28 text-3xl'>$0</p>
+              </div>
+
+            </div>
+            <div className='card border flex h-[120px] w-[400px] p-8 relative'>
+              <img src={trade} alt="" className='h-12' />
+              <div>
+                <p className='absolute left-28'>Total Trading Volume</p>
+                <p className='absolute top-14 left-28 text-3xl'>$0</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className='border w-[100%] absolute top-[750px]'></div>
+      </div>
+
+    </div>
   )
 }
 
-export default Home
+export default Home;
