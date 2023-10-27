87bb4b27fff2dbb4cf11e80a160bc22192f03106
thimanshu756 <himanshu1662.be22@gmail.com>
2023-10-27 17:47:39 +0530

    first page2

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import logo from "./Logo.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className='border w-[1500px]'> 
+      </div>
+      <div className='flex items-center  absolute left-[500px] w-[400px]'>
+        <img src={logo} alt="" className='h-[100px]'/>
+        <h1 className='absolute right-[120px] text-4xl'>Romi Finance</h1>
+
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>       
+           </ul>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,20 @@ import Navbar from '../Components/Navbar';
 import costimg from './costs.svg';
 import trade from './trading.svg';
 import interest from './interest.svg';
+import liquid_img from './liquid.svg';
+import swap_img from './swap.svg';
+import zlp_img from './zlp.png';
+import zomi_img from './zomi.png';
+import Footer from '../Components/Footer';
 const Home = () => {
   return (
-    <div className='bg-black  h-[1000px] text-white'>
+    <div className='bg-black  h-[2000px] text-white'>
       <div>
         <Navbar />
       </div>
       <div className='bg-black '>
         <div className='Sec absolute top-[120px] left-[100px] '>
+          {/* Tagline */}
           <h1 className='text-6xl font-bold'>Decentralized <br />
             Perpetual Exchange</h1> <br />
           <p className='text-xl'>Trade BTC, ETH, AVAX and other top <br /> cryptocurrencies with up to 30x leverage <br /> directly from your wallet</p> <br />
@@ -43,9 +49,74 @@ const Home = () => {
         </div>
         <div className='border w-[100%] absolute top-[750px]'></div>
       </div>
+        {/* big cards */}
+        <div className='flex top-[850px] absolute left-[100px] w-[100%] gap-[200px]'>
+          <div className='service-card  '>
+          <span className='flex gap-4'>
+            <img src={liquid_img} alt="" />
+            <h1 className='text-xl'>Reduce Liquidation Risks</h1>
+          </span>
+          <p className='pt-4'>
+          An aggregate of high-quality price <br /> feeds determine when liquidations <br /> occur. This keeps positions safe <br />from temporary wicks.
+          </p>
+          </div>
+          <div className='service-card  '>
+          <span className='flex gap-4'>
+            <img src={costimg} alt="" />
+            <h1 className='text-xl'>Save on Costs</h1>
+          </span>
+          <p className='pt-4'>
+          Enter and exit positions with <br /> minimal spread and zero price <br /> impact. Get the optimal price <br /> without incurring additional costs.
+          </p>
+          </div>
+          <div className='service-card  '>
+          <span className='flex gap-4'>
+            <img src={swap_img} alt="" />
+            <h1 className='text-xl'>Simple Swaps</h1>
+          </span>
+          <p className='pt-4'>
+          Open positions through a simple <br /> swap interface. Conveniently swap <br />from any supported asset into the <br />position of your choice.
+          </p>
+          </div>
+        </div>
+        {/* tokens sections */}
+        <div className='absolute tokens-sec  top-[1150px] left-[100px]'>
+        <h1 className='text-5xl font-bold'>Two tokens create <br /> our ecosystem</h1>
+        <div className='flex mt-10 gap-7'>
+        <div className='token-cards border w-[600px] h-[250px] rounded pl-6 pt-5 relative'>
+        <div className='flex gap-3'>
+        <img src={zomi_img} alt="" className='h-[40px]' />
+        <h1 className='text-xl absolute top-6 left-[70px]'>$ZOMI</h1>
+        </div>
+        <p className='pt-5 text-xl'>$ZOMI is the utility and governance token. Accrues 30% of the <br />platform's generated fees.</p>
+        <br />
+        <span className=''>Ethereum APR: 0.00%</span>
+        <div className='flex gap-6 pt-4'>
+          <button className='w-20 rounded  bg-green-500'>Buy</button>
+          <button className='border w-28 h-7 rounded'>Read more</button>
+        </div>
+        </div>
 
+
+        <div className='token-cards border w-[600px] h-[250px] rounded pl-6 pt-5 relative'>
+        <div className='flex gap-3'>
+        <img src={zlp_img} alt="" className='h-[40px]' />
+        <h1 className='text-xl absolute top-6 left-[70px]'>$ZLP</h1>
+        </div>
+        <p className='pt-5 text-xl'>$ZLP is the liquidity provider token. Accrues 70% of the <br /> platform's generated fees.</p>
+        <br />
+        <span className=''>Ethereum APR: 1,655,928,224.10%</span>
+        <div className='flex gap-6 pt-4'>
+          <button className='w-20 rounded  bg-green-500'>Buy</button>
+          <button className='border w-28 h-7 rounded'>Read more</button>
+        </div>
+        </div>
+        </div>
+        </div>
+        <div className='absolute top-[1800px]'>
+         <Footer/>
+        </div>
     </div>
   )
 }
-
 export default Home;
